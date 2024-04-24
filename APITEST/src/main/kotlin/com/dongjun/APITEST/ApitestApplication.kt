7a344e43e6f5496cbdf10aae506fb174e35e4018
Dongjun320 <155514648+Dongjun320@users.noreply.github.com>
2024-04-24 15:03:39 +0900

    Initial commit

---
+++ APITEST/src/main/kotlin/com/dongjun/APITEST/ApitestApplication.kt
@@ -0,0 +1,11 @@
+package com.dongjun.APITEST
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApitestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApitestApplication>(*args)
+}
---
+++ APITEST/src/test/kotlin/com/dongjun/APITEST/ApitestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dongjun.APITEST
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApitestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
